4af08214c916e221d353e05e128e09d69a04a56f
Soumen Mondal <mondalsoumen00@gmail.com>
2025-07-15 12:19:38 +0530

    Time: 45 ms (48.16%), Space: 32.6 MB (54.41%) - LeetArchive

---
+++ LeetCode/1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,48 @@
+class DisjointSet:
+    def __init__(self, n: int):
+        self.n = n
+        self.parent = [i for i in range(n)]
+        self.size = [1 for i in range(n)]
+    
+    def find(self, u: int) -> int:
+        pu = self.parent[u]
+        if pu == u:
+            return u
+        else:
+            upu = self.find(pu)
+            self.parent[u] = upu
+            return upu
+    
+    def union(self, u: int, v: int):
+        u = self.find(u)
+        v = self.find(v)
+        if u != v:
+            if self.size[u] >= self.size[v]:
+                self.parent[v] = u
+                self.size[u] += self.size[v]
+            elif self.size[u] < self.size[v]:
+                self.parent[u] = v
+                self.size[v] += self.size[u]
+    
+    def num_comp(self) -> int:
+        count = 0
+        for i in range(self.n):
+            if i == self.parent[i]:
+                count += 1
+        return count
+
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        ds = DisjointSet(n)
+        A = []
+        for u, v in connections:
+            if ds.find(u) == ds.find(v):
+                A.append((u, v))
+            else:
+                ds.union(u, v)
+        
+        k = ds.num_comp()
+        if len(A) >= k - 1:
+            return  k - 1
+        else:
+            return -1
