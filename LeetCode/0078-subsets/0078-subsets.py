9c3224810833303a21962f5147cfc8ddf7322960
Soumen Mondal <mondalsoumen00@gmail.com>
2025-07-17 17:26:25 +0530

    Time: 4 ms (4.49%), Space: 17.9 MB (40.12%) - LeetArchive

---
+++ LeetCode/0078-subsets/0078-subsets.py
@@ -0,0 +1,28 @@
+class Solution:
+    def subsetsFunctional(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        def func(idx: int) -> List[List[int]]:
+            if idx >= n:
+                return [[]]
+            else:
+                res = func(idx+1) # [[], [2], [3], [2,3]]
+                combinedRes = []
+                for r in res:
+                    combinedRes.append(r)
+                    combinedRes.append([nums[idx]] + r)
+                return combinedRes
+        return func(0)
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        def func(idx: int, currentSubseq: List[int]):
+            if idx >= n:
+                res.append(currentSubseq.copy())
+            else:
+                currentSubseq.append(nums[idx])
+                func(idx+1, currentSubseq)
+                currentSubseq.pop()
+                func(idx+1, currentSubseq)
+        func(0, [])
+        return res
