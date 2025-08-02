91ca6a5044be134f5afd0f9a28b2f71af69ed2e4
Soumen Mondal <mondalsoumen00@gmail.com>
2025-08-02 19:46:14 +0530

    Time: 10836 ms (5%), Space: 237 MB (5.01%) - LeetArchive

---
+++ LeetCode/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        DP = [[-1 for i in range(n+1)] for i in range(n)]
+        def func(index: int, prev: int) -> int:
+            if index == n:
+                return 0
+            if DP[index][prev+1] != -1:
+                return DP[index][prev+1] 
+            resNotTake = func(index+1, prev)
+            resTake = -math.inf
+            if prev == -1 or nums[index] > nums[prev]:
+                resTake = 1 + func(index+1, index)
+            res = max(resTake, resNotTake)
+            DP[index][prev+1] = res
+            return res
+        return func(0, -1)
