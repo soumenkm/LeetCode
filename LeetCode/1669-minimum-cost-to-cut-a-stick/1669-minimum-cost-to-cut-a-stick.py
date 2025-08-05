a32ad47bf7b0c548ab035174dfad2a20ee98e827
Soumen Mondal <mondalsoumen00@gmail.com>
2025-08-05 20:13:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetArchive

---
+++ LeetCode/1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minCost(self, n: int, cuts: List[int]) -> int:
+        arr = range(n+1)
+        DP = {}
+        def func(i: int, j: int) -> int:
+            if j == i + 1:
+                return 0
+            
+            if (i, j) in DP:
+                return DP[(i, j)]
+            res = []
+            for k in cuts:
+                if k > i and k < j:
+                    resLeft = func(i, k)
+                    resRight = func(k, j)
+                    tempRes = j - i + max(resLeft + resRight, 0)
+                    res.append(tempRes)
+                    print(i, k, j, tempRes, resLeft, resRight)
+            res = [r for r in res if r > 0]
+            finalRes = min(res) if res else 0
+            DP[(i, j)] = finalRes
+            return finalRes
+        
+        return func(0, n)
+            
