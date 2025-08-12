87df03c60a2b3a03c912c351dbca9a348343c70c
Soumen Mondal <mondalsoumen00@gmail.com>
2025-08-12 13:51:19 +0530

    Time: 91 ms (28.91%), Space: 33 MB (6.12%) - LeetArchive

---
+++ LeetCode/0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        DP = [None for i in range(n)]
+        DP[0] = nums[0]
+        for i in range(1, len(nums)):
+            DP[i] = max(nums[i], nums[i] + DP[i-1])
+        return max(DP)
