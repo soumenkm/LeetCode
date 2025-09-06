30337a1c9c1cd1e5e1b4888ece9422e2e13c7d6b
Soumen Mondal <mondalsoumen00@gmail.com>
2025-09-06 14:59:56 +0530

    Time: 7 ms (75.41%), Space: 18.2 MB (9.85%) - LeetArchive

---
+++ LeetCode/0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        def func(n: int) -> str:
+            if n == 1:
+                return "1"
+            
+            string = func(n-1)
+            n = len(string)
+            i = 0
+            res = []
+            while i < n:
+                count = 0
+                start = i
+                while i < n and string[start] == string[i]:
+                    count += 1
+                    i += 1
+                res.append(str(count))
+                res.append(string[start])
+            
+            return "".join(res)
+        
+        return func(n)
