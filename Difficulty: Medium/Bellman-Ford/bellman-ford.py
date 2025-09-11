d02a94d400292774680a1ef4c4c97b2ca46127b2
Soumen Mondal <mondalsoumen00@gmail.com>
2025-09-11 23:25:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bellman-Ford/bellman-ford.py
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.py
@@ -5,19 +5,21 @@ class Solution:
         dist = [float("inf") for i in range(V)]
         dist[src] = 0
         
-        def relax(u: int, v: int, w: int) -> bool:
-            if dist[u] + w < dist[v]:
-                dist[v] = dist[u] + w
+        def relax(dist_u: int, v: int, w: int) -> bool:
+            if dist_u + w < dist[v]:
+                dist[v] = dist_u + w
                 return True
             else:
                 return False
         
         for _ in range(V-1):
+            tempDist = dist.copy()
             for  u, v, w in edges:
-                relax(u, v, w)
+                relax(tempDist[u], v, w)
         
+        tempDist = dist.copy()
         for u, v, w in edges:
-            if relax(u, v, w):
+            if relax(tempDist[u], v, w):
                 return [-1]
         
         dist = [10**8 if d == float("inf") else d for d in dist]
