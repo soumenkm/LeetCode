c41dc7c54d5a4f495599fba3b9f8c43aeb809f32
Soumen Mondal <mondalsoumen00@gmail.com>
2025-09-14 21:05:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.py
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.py
@@ -3,23 +3,27 @@ class Solution:
     # Function to return Breadth First Search Traversal of given graph.
     def bfs(self, adj):
         n = len(adj)
+        res = []
         color = [0 for i in range(n)]
         
-        Q = deque()
-        Q.append(0)
-        color[0] = 1
-        res = []
+        def BFS(src: int):
+            Q = deque()
+            Q.append(src)
+            color[src] = 1
+            
+            while len(Q) != 0:
+                size = len(Q)
+                for _ in range(size):
+                    node = Q.popleft()
+                    res.append(node)
+                    for adjNode in adj[node]:
+                        if color[adjNode] == 0:
+                            color[adjNode] = 1
+                            Q.append(adjNode)
         
-        while len(Q) != 0:
-            size = len(Q)
-            for i in range(size):
-                node = Q.popleft()
-                res.append(node)
-                
-                for adjNode in adj[node]:
-                    if color[adjNode] == 0:
-                        color[adjNode] = 1
-                        Q.append(adjNode)
+        for i in range(n):
+            if color[i] == 0:
+                BFS(i)
         
         return res
         
