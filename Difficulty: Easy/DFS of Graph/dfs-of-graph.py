751b8ffd9460acf1608d16eaaaacbca361050eba
Soumen Mondal <mondalsoumen00@gmail.com>
2025-09-14 21:01:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.py
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.py
@@ -2,27 +2,18 @@ class Solution:
     
     #Function to return a list containing the DFS traversal of the graph.
     def dfs(self, adj):
-        res = []
         n = len(adj)
-        start = [0] * n
-        finish = [0] * n
-        color = [0] * n
-        time = 0
-        
-        def DFS(node: int) -> None:
-            nonlocal time
+        color = [0 for i in range(n)]
+        res = []
+        def DFS(node: int):
             color[node] = 1
-            time += 1
-            start[node] = time
             res.append(node)
-            
-            for adjacent in adj[node]:
-                if color[adjacent] == 0:
-                    DFS(adjacent)
-            
-            time += 1
-            finish[node] = time
-            color[node] = 2
+            for adjNode in adj[node]:
+                if color[adjNode] == 0:
+                    DFS(adjNode)
         
-        DFS(0)
+        for i in range(n):
+            if color[i] == 0:
+                DFS(i)
+        
         return res
