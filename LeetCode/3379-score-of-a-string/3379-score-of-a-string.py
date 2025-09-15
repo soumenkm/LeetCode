80e6822070b20dc51cc842dbf9e839bd1a7d1ecd
Soumen Mondal <mondalsoumen00@gmail.com>
2025-09-15 23:38:21 +0530

    Time: 0 ms (100%), Space: 17.7 MB (78.17%) - LeetArchive

---
+++ LeetCode/3379-score-of-a-string/3379-score-of-a-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        n = len(s)
+        for i in range(1, n):
+            score += abs(ord(s[i]) - ord(s[i-1]))
+        return score
