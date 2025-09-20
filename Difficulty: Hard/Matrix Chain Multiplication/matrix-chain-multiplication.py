4db705cf8333090f9efd569820eb79ba024ce297
Soumen Mondal <mondalsoumen00@gmail.com>
2025-09-20 18:35:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
@@ -1,16 +1,20 @@
 from functools import cache
 class Solution:
     def matrixMultiplication(self, arr):
-        n = len(arr)
+        # code here
         @cache
-        def mcm(i: int, j: int) -> int:
-            if j-i == 1:
+        def func(i: int, j: int) -> int:
+            if j - i == 1:
                 return 0
-                
+            
             mini = float("inf")
             for k in range(i+1, j):
-                mult = mcm(i, k) + mcm(k, j) + arr[i] * arr[j] * arr[k]
+                mult = func(i, k) + func(k, j) + arr[i] * arr[k] * arr[j]
                 mini = min(mini, mult)
+            
             return mini
         
-        return mcm(0, n-1)
+        return func(0, len(arr) - 1)
+            
+        
+        
