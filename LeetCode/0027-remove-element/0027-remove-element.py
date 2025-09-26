027cda8a89b2a6895d262f79af8cd2b7c8933841
Soumen Mondal <mondalsoumen00@gmail.com>
2025-09-26 20:40:17 +0530

    Time: 0 ms (100%), Space: 17.7 MB (52.41%) - LeetArchive

---
+++ LeetCode/0027-remove-element/0027-remove-element.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        n = len(nums)
+        i = 0
+        j = n - 1
+
+        count = 0
+        while j >= 0 and i <= n-1 and i <= j:
+            if nums[j] == val:
+                j -= 1
+                count += 1
+                continue
+                
+            if nums[i] != val:
+                i += 1
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                j -= 1
+                count += 1
+
+        return n - count
