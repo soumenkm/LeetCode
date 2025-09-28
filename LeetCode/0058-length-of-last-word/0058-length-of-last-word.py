42e47a5d6f3eed472132c17d19995ece801fa9e6
Soumen Mondal <mondalsoumen00@gmail.com>
2025-09-28 20:58:47 +0530

    Time: 0 ms (100%), Space: 17.8 MB (61.22%) - LeetArchive

---
+++ LeetCode/0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i = len(s)-1
+        while s[i] == " ":
+            i -= 1
+        
+        count = 0
+        while i >= 0 and s[i] != " ":
+            i -= 1
+            count += 1
+        
+        return count
